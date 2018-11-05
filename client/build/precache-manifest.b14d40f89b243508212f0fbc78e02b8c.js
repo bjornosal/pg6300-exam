3dc45224b4a0737f4ee70fca53055745af2d3822
bjornosal <bjorn.olav.salvesen@gmail.com>
2018-11-05 10:44:33 +0100

    Added build, as needed for heroku.

---
+++ client/build/precache-manifest.b14d40f89b243508212f0fbc78e02b8c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "00c9c62ddf39691b2d73",
+    "url": "/static/js/main.00c9c62d.chunk.js"
+  },
+  {
+    "revision": "621fefbff32588882b7d",
+    "url": "/static/js/1.621fefbf.chunk.js"
+  },
+  {
+    "revision": "00c9c62ddf39691b2d73",
+    "url": "/static/css/main.b2c6b575.chunk.css"
+  },
+  {
+    "revision": "958a5935b659e20c8a34d8d3488c8c35",
+    "url": "/index.html"
+  }
+];
