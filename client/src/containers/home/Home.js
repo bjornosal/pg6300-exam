0e3dfe9a058a68c908fa58d3ebda0b7dac0908bf
bjornosal <bjorn.olav.salvesen@gmail.com>
2018-11-07 17:52:25 +0100

    Added css for home page

--- client/src/containers/home/Home.js
+++ client/src/containers/home/Home.js
@@ -1,14 +1,34 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
-import { connect } from 'react-redux'
-import { Link } from 'react-router-dom'
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import questionmark from "./../../questionMark.png";
+import listIcon from "./../../listIcon.svg";
 
 export default class Home extends Component {
   render() {
     return (
       <div className="homeContainer">
-        <h2>Home</h2>
+        <div className="gameModeContainer">
+          <div className="gamemodeChooser gameModeAuto">
+            <div className="gameModeImageContainer">
+              <img className="gameModeImage" src={questionmark} />
+            </div>
+            <p className="gameModeInfo">Automatically join a game. </p>
+            <button className="gamemodeButton">Auto</button>
+          </div>
+          <div className="gamemodeChooser gameModeLobby">
+            <div className="gameModeImageContainer">
+              <img className="gameModeImage" src={listIcon} />
+            </div>
+            <p className="gameModeInfo">
+              Go to the lobby to find a game there!
+              <br /> Are your friends playing?
+            </p>
+            <button className="gamemodeButton">Lobby</button>
+          </div>
+        </div>
       </div>
-    )
+    );
   }
 }
