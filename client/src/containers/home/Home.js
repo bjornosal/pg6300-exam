f0a8b3c44f8e57be1d8bd9dfa76df3c95592c2d1
bjornosal <bjorn.olav.salvesen@gmail.com>
2018-11-07 18:03:11 +0100

    Added some minor styling and fixes.

--- client/src/containers/home/Home.js
+++ client/src/containers/home/Home.js
@@ -5,30 +5,30 @@ import { Link } from "react-router-dom";
 import questionmark from "./../../questionMark.png";
 import listIcon from "./../../listIcon.svg";
 
-export default class Home extends Component {
-  render() {
-    return (
-      <div className="homeContainer">
-        <div className="gameModeContainer">
-          <div className="gamemodeChooser gameModeAuto">
-            <div className="gameModeImageContainer">
-              <img className="gameModeImage" src={questionmark} />
-            </div>
-            <p className="gameModeInfo">Automatically join a game. </p>
-            <button className="gamemodeButton">Auto</button>
+const Home = () => {
+  return (
+    <div className="homeContainer">
+      <div className="gameModeContainer">
+        <div className="gamemodeChooser gameModeAuto">
+          <div className="gameModeImageContainer">
+            <img className="gameModeImage" src={questionmark} />
           </div>
-          <div className="gamemodeChooser gameModeLobby">
-            <div className="gameModeImageContainer">
-              <img className="gameModeImage" src={listIcon} />
-            </div>
-            <p className="gameModeInfo">
-              Go to the lobby to find a game there!
-              <br /> Are your friends playing?
-            </p>
-            <button className="gamemodeButton">Lobby</button>
+          <p className="gameModeInfo">Automatically join a game. </p>
+          <Link to="/game" className="gamemodeButton">Auto</Link>
+        </div>
+        <div className="gamemodeChooser gameModeLobby">
+          <div className="gameModeImageContainer">
+            <img className="gameModeImage" src={listIcon} />
           </div>
+          <p className="gameModeInfo">
+            Go to the lobby to find a game there!
+            <br /> Are your friends playing?
+          </p>
+          <Link to="/lobby" className="gamemodeButton">Lobby</Link>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
+
+export default Home;
