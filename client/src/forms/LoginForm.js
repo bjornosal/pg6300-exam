08d5ddfb5622e926b1d3ed5c699f93ef9b30fe22
bjornosal <bjorn.olav.salvesen@gmail.com>
2018-11-20 20:28:53 +0100

    Added error message to login

--- client/src/forms/LoginForm.js
+++ client/src/forms/LoginForm.js
@@ -3,57 +3,64 @@ import { Field, reduxForm } from "redux-form";
 import InputField from "../components/inputField/InputField";
 import { connect } from "react-redux";
 import { loginUserAsync } from "../actions/Login";
-import { withRouter } from 'react-router-dom'
+import { withRouter } from "react-router-dom";
 /**
  * Inspired by Redux-Form examples - https://redux-form.com/7.4.2/examples/asyncvalidation/
  */
 
 class LoginForm extends React.Component {
-
-  onLogin = (fields) =>  {
-    this.props.loginUserAsync(fields, this.props.history)
-  }
+  onLogin = fields => {
+    this.props.loginUserAsync(fields, this.props.history);
+  };
 
   componentDidUpdate = () => {
-    if(this.props.loggedIn) {
+    if (this.props.loggedIn) {
       this.props.history.push("/");
     }
-  }
+  };
 
   render() {
     const { handleSubmit, isSubmitting } = this.props;
     return (
-      <form onSubmit={handleSubmit(this.onLogin)}>
-        <Field
-          name="username"
-          type="text"
-          component={InputField}
-          label="Username"
-        />
-        <Field
-          name="password"
-          type="password"
-          component={InputField}
-          label="Password"
-        />
-        <div className="loginButtonContainer">
-          <button type="submit" disabled={isSubmitting}>
-            Log in
-          </button>
-        </div>
-      </form>
+      <div>
+        <form onSubmit={handleSubmit(this.onLogin)}>
+          <Field
+            name="username"
+            type="text"
+            component={InputField}
+            label="Username"
+          />
+          <Field
+            name="password"
+            type="password"
+            component={InputField}
+            label="Password"
+          />
+          <div className="loginButtonContainer">
+            <button type="submit" disabled={isSubmitting}>
+              Log in
+            </button>
+          </div>
+        </form>
+        {this.props.loginError && (
+          <div className="errorBox">Unable to log in</div>
+        )}
+      </div>
     );
-  };
+  }
 }
 
 const formName = "login";
 
-const mapStateToProps = (state) => {
+const mapStateToProps = state => {
   return {
-    loggedIn: state.login ?  state.login.loggedIn : false,    
-    user: state.form[formName] ? state.form[formName].registeredFields : undefined
+    loggedIn: state.login ? state.login.loggedIn : false,
+    user: state.form[formName]
+      ? state.form[formName].registeredFields
+      : undefined,
+      loginError: state.login ? state.login.loginError : undefined
   };
-}
+};
 
 export default reduxForm({
   form: formName
